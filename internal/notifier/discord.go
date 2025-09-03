2e01f951b3442a7b40529f1179917332be68a56f
Shishir Pai <64056966+mgpai22@users.noreply.github.com>
2025-09-03 17:42:51 -0400

    feat: add discord notification helpers for node healthcheck

--- internal/notifier/discord.go
+++ internal/notifier/discord.go
@@ -2,26 +2,66 @@ package notifier
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/disgoorg/disgo/discord"
 )
 
-func BuildDiscordMessage(
-	baseURI string,
-	amountSent int,
-	txHash string,
-	currentBlockHeight uint64,
+func BuildNodeHealthAlert(
+	consecutiveFailures int,
+	maxFailures int,
+	errorMessage string,
+	responseTime int64,
+	network string,
 ) discord.WebhookMessageCreate {
 	embed := discord.NewEmbedBuilder().
-		SetColor(0xd9dadb).
-		SetAuthor("template", "", "https://i.imgur.com/1xbxWrX.png").
-		SetTitle(fmt.Sprintf("%s ADA Sent", formatTokenAmount(amountSent))).
-		SetURL(baseURI+txHash).
-		SetThumbnail("https://i.imgur.com/XILpfxc.png").
-		SetFooter(fmt.Sprintf("Block %d", currentBlockHeight), "https://i.imgur.com/rzmSALh.png")
+		SetColor(0xff0000).
+		SetAuthor("Sigmalok Indexer", "", "https://i.imgur.com/1xbxWrX.png").
+		SetTitle("🚨 CRITICAL: Node Health Failure").
+		SetDescription(fmt.Sprintf(
+			"Node health check has failed **%d/%d** consecutive times.\n\n"+
+				"**Error:** %s\n"+
+				"**Response Time:** %dms\n"+
+				"**Network:** %s",
+			consecutiveFailures,
+			maxFailures,
+			errorMessage,
+			responseTime,
+			network,
+		)).
+		SetTimestamp(time.Now()).
+		SetFooter("Sigmalok Health Monitor", "https://i.imgur.com/rzmSALh.png")
 
 	return discord.NewWebhookMessageCreateBuilder().
-		SetUsername("4eyes").
+		SetUsername("Sigmalok Monitor").
+		SetAvatarURL("https://pbs.twimg.com/profile_images/1770426683339755520/eRHY1Ex5_400x400.jpg").
+		SetEmbeds(embed.Build()).
+		Build()
+}
+
+func BuildHeightStagnationAlert(
+	currentHeight uint64,
+	stagnationDuration time.Duration,
+	limit time.Duration,
+	network string,
+) discord.WebhookMessageCreate {
+	embed := discord.NewEmbedBuilder().
+		SetColor(0xff6600).
+		SetAuthor("Sigmalok Indexer", "", "https://i.imgur.com/1xbxWrX.png").
+		SetTitle("⚠️ CRITICAL: Node Height Stagnation").
+		SetDescription(fmt.Sprintf(
+			"Node height has not increased for **%s** (limit: %s).\n\n"+
+				"**Current Height:** %d\n"+
+				"**Stagnation Duration:** %s\n"+
+				"**Network:** %s\n\n"+
+				"This may indicate the node is not syncing properly with the blockchain.",
+			stagnationDuration.Round(time.Second), limit,
+			currentHeight, stagnationDuration.Round(time.Second), network)).
+		SetTimestamp(time.Now()).
+		SetFooter("Sigmalok Health Monitor", "https://i.imgur.com/rzmSALh.png")
+
+	return discord.NewWebhookMessageCreateBuilder().
+		SetUsername("Sigmalok Monitor").
 		SetAvatarURL("https://pbs.twimg.com/profile_images/1770426683339755520/eRHY1Ex5_400x400.jpg").
 		SetEmbeds(embed.Build()).
 		Build()
